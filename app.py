8b7d00941954282d41eead9e3a9e5bb0d5c3f1bb
Nishitha Tirumalaraju <152367167+nishithat-28@users.noreply.github.com>
2024-11-13 17:28:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pickle model
+model = pickle.load(open('wine_quality_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html', prediction=None)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get form data
+    volatile_acidity = float(request.form['volatile acidity'])
+    chlorides = float(request.form['chlorides'])
+    total_sulfur_dioxide = float(request.form['total sulfur dioxide'])
+    density = float(request.form['density'])
+    sulphates = float(request.form['sulphates'])
+    alcohol = float(request.form['alcohol'])
+
+    # Create a numpy array with the form data
+    data = np.array([[volatile_acidity,chlorides,total_sulfur_dioxide,density,sulphates,alcohol]])
+
+    # Make prediction using the loaded model
+    prediction = model.predict(data)[0]
+
+    # Render the template with the prediction
+    return render_template('index.html', prediction=round(prediction, 2))
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host='0.0.0.0',port=8000)
